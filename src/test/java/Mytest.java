1ced53c225c406108ab8ccb4cb804d224ce45590
Sachin <sachinprasad1998mav@gmail.com>
2024-03-31 13:11:28 -0500

    added TestNG dependency and implemented TestNG testing scaenario
    I have used TestNG to test and have generated a test report which can be viewed once run the XML file or can view the attached screen shot

--- src/test/java/Mytest.java
+++ src/test/java/Mytest.java
@@ -5,48 +5,85 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.testng.annotations.*;
 
 public class Mytest
 {
-    public static void main(String[] args) throws InterruptedException {
-//      Creating a new FirefoxDriver
-        WebDriver driver = new FirefoxDriver();
+    WebDriver driver;
+
+//        Creating a new FirefoxDriver
+    @BeforeSuite
+        public void createDriver()
+    {
+        driver = new FirefoxDriver();
+    }
+
+
+    @Test(priority = 1)
+    public void accessAmazon()
+    {
 //      accessing amazon website
         driver.get("https://www.amazon.com/");
+    }
 
-        //searches for the search box and enters text stanley cup and clicks submit button
+    @Test(priority = 2)
+    public void searchAndSubmitStanley()
+    {
+//      searches for the search box and enters text stanley cup and clicks submit button
         WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
         enterText(driver, 10, "Stanley cup", searchBox);
         WebElement searchButton = driver.findElement(By.id("nav-search-submit-button"));
         clickButton(driver, 10, searchButton);
+    }
 
-        //filters the products with only products having ratings 3 stas and more and clicks on the first product
+    @Test(priority = 3)
+    public void filterProducts()
+    {
+//      filters the products with only products having ratings 3 stas and more and clicks on the first product
         WebElement threeStar = driver.findElement(By.xpath("//i[@class='a-icon a-icon-star-medium a-star-medium-3']"));
         clickButton(driver, 10, threeStar);
         WebElement productOne = driver.findElement(By.xpath("//img[@alt='Stanley Quencher H2.0 FlowState Stainless Steel Vacuum Insulated Tumbler with Lid and Straw for Water, Iced Tea or Coffee']"));
         clickButton(driver, 10, productOne);
+    }
 
-        //adds the product to cart and enters cart page
+    @Test(priority = 4)
+    public void addProduct()
+    {
+//        adds the product to cart and enters cart page
         WebElement addToCart = driver.findElement(By.id("add-to-cart-button"));
         clickButton(driver, 10, addToCart);
         WebElement goToCart = driver.findElement(By.xpath("//a[@href='/cart?ref_=sw_gtc']"));
         clickButton(driver, 10, goToCart);
+    }
 
-        //suddenly goes to youtube and searches for a cinnamon latte receipe
+    @Test(priority = 5)
+    public void accessYoutube()
+    {
+
+//        suddenly goes to youtube and searches for a cinnamon latte receipe
         driver.navigate().to("https://www.youtube.com/");
         WebElement youtubeSearch = driver.findElement(By.name("search_query"));
         enterText(driver, 10, "cinnamon latte receipe", youtubeSearch);
         WebElement youtubeSearchButton = driver.findElement(By.xpath("//button[@id='search-icon-legacy']"));
         clickButton(driver, 10, youtubeSearchButton);
+    }
 
-        //goes back to amazon cart and deletes the cart and thus ending the testing phase.
+    @Test(priority = 6)
+    public void gobackAmazon()
+    {
+//      goes back to amazon cart and deletes the cart and thus ending the testing phase.
         driver.navigate().back();
         WebElement deleteIcon =  driver.findElement(By.className("a-button a-button-base a-button-small"));
         clickButton(driver, 10, deleteIcon);
+    }
 
-        Thread.sleep(4000);
+
+    @AfterSuite
+    public void quitDriver()
+    {
         driver.quit();
     }
+
 
 
     public static void enterText(WebDriver driver, int timeout, String text, WebElement element)
